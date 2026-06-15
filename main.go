b59671d893bf5d84b6741e9f611e91c1b788d99f
Arun Paudel <aroon.paudel@gmail.com>
2026-06-15 12:32:23 -0700

    refactor: the repl logic into it's own func

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := scanner.Text()
-		clean := cleanInput(input)
-		fmt.Printf("Your command was: %v\n", clean[0])
-	}
+	startRepl()
 }
